5b4e77d06e41b8ee2b65bf9c22c81d24219c78fc
Eric <ehchao88@gmail.com>
2019-09-21 14:36:12 -0400

    oops

--- scripts/image_search.py
+++ scripts/image_search.py
@@ -2,6 +2,6 @@ import requests
 import json
 #AIzaSyDDISWLsMtifIhV0W6dGu_TyN-fJcQx3Zo
 def get_image_url(query):
-	search_result = requests.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyCErOgI_eajWUgatZpNABsjBrauT2RSE50&cx=003789856186025432586:pbxx5vdz2go&searchType=image&q=' + query)
+	search_result = requests.get('https://www.googleapis.com/customsearch/v1?key={Insert Key}&cx=003789856186025432586:pbxx5vdz2go&searchType=image&q=' + query)
 	search_result_dict = json.loads(search_result.text)
 	return str(search_result_dict['items'][0]['link'])
